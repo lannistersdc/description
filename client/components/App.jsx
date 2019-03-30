900633e66f87ba8d66be3629350b44f83cbefac5
Uttej Gujral <uttej@Uttejs-MacBook-Pro.local>
2019-03-30 12:04:49 -0700

    [ fix ] Fixed App skelton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,40 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable-next-line prefer-const */
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      restaurants: [],
+      restaurant: [],
     };
+    this.getOneRestaurant = this.getOneRestaurant.bind(this);
+  }
+
+  componentDidMount() {
+    this.getOneRestaurants();
+  }
+
+  getOneRestaurant() {
+    let { id } = req.params;
+    axios
+      .get('/api/restaurants/:id')
+      .then((response) => {
+        this.setState({
+          restaurant: response.data,
+        });
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
     return (
-      <div />
+      <div className="main-app" />
     );
   }
 }
