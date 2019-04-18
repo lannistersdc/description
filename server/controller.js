df33ae6311f2e9d0d5c3969ec91fc7d3ce082d3d
Calvin Yao <calvin.yao21@gmail.com>
2019-04-18 16:04:17 -0700

    [feat] added CRUD routes to server

--- server/controller.js
+++ server/controller.js
@@ -7,9 +7,33 @@ const controller = {
       .then((docs) => {
         res.status(200).send(docs);
       })
-      // eslint-disable-next-line no-console
       .catch(err => console.error(err));
   },
+  post: (req, res) => {
+    const { restaurant } = req.body;
+    Opentable.create(restaurant)
+      .then((docs) => {
+        res.status(201).send(docs);
+      })
+      .catch(err => console.error(err));
+  },
+  put: (req, res) => {
+    const { restaurantId } = req.params;
+    const { restaurant } = req.body;
+    Opentable.findOneAndUpdate({ restaurantId }, restaurant, {new: true})
+      .then((docs) => {
+        res.status(204).send(docs);
+      })
+      .catch(err => console.error(err));
+  },
+  delete: (req, res) => {
+    const { restaurantId } = req.params;
+    Opentable.findOneAndDelete({ restaurantId })
+      .then((docs) => {
+        res.sendStatus(204);
+      })
+      .catch(err => console.error(err));
+  }
 };
 
 module.exports = controller;
