14ab8a3b2f439b402f1d75f9adb926fae5b69c1a
Calvin Yao <calvin.yao21@gmail.com>
2019-04-26 15:18:24 -0700

    [prog] added 'allow null' to schemas to prevent possible errors

--- database/psql/model.js
+++ database/psql/model.js
@@ -5,42 +5,53 @@ const DescriptionModel = db.define(
   'Description',
   {
     restaurantId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    restaurantName: { type: Sequelize.STRING, allowNull: false },
-    restaurantRating: { type: Sequelize.FLOAT, allowNull: false },
-    restaurantReviews: { type: Sequelize.INTEGER },
-    restaurantPrice: { type: Sequelize.STRING },
-    restaurantCuisine: { type: Sequelize.STRING },
-    restaurantDescription: { type: Sequelize.STRING },
-    restaurantTags: { type: Sequelize.ARRAY(Sequelize.STRING) },
-    restaurantPhotos: { type: Sequelize.ARRAY(Sequelize.STRING) }
+    restaurantName: { type: Sequelize.STRING, allowNull: true },
+    restaurantRating: { type: Sequelize.FLOAT, allowNull: true },
+    restaurantReviews: { type: Sequelize.INTEGER, allowNull: true },
+    restaurantPrice: { type: Sequelize.STRING, allowNull: true },
+    restaurantCuisine: { type: Sequelize.STRING, allowNull: true },
+    restaurantDescription: { type: Sequelize.STRING(600), allowNull: true },
+  },
+  {
+    timestamps: false
   }
 );
 
-const TagModel = db.define('Tag', {
-  tag: { type: Sequelize.STRING },
-  restaurantId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: 'Descriptions', 
-      key: 'restaurantId' 
+const TagModel = db.define('Tag', 
+  {
+    tag: { type: Sequelize.STRING, allowNull: true },
+    restaurantId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Descriptions', 
+        key: 'restaurantId' 
+      }
     }
+  },
+  {
+    timestamps: false
   }
-})
+);
 
-const PhotoModel = db.define('Photo', {
-  photo: { type: Sequelize.STRING },
-  restaurantId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: 'Descriptions', 
-      key: 'restaurantId' 
+const PhotoModel = db.define('Photo', 
+  {
+    photo: { type: Sequelize.STRING, allowNull: true },
+    restaurantId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Descriptions', 
+        key: 'restaurantId' 
+      }
     }
+  },
+  {
+    timestamps: false
   }
-})
+)
 
 
 db
-  .sync()
+  .sync({force: true})
   .then(() => { console.log('Synced!') })
   .catch(error => console.error(error));
 
